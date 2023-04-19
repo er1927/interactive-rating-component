0c3f699d6a3262935c588bc1b84390d09eeb4a6a
Elizabeth R <elizabeth.roberts1927@gmail.com>
2023-04-19 16:10:29 -0300

    Added image on thank you card display.

--- script.js
+++ script.js
@@ -4,33 +4,35 @@ const three = document.getElementById("three");
 const four = document.getElementById("four");
 const five = document.getElementById("five");
 const star = document.getElementById("star-container");
+const thankYouImg = document.getElementById("thank-you-img");
+const yourSelection = document.getElementById("your-selection");
 const title= document.getElementById("title");
 const text = document.getElementById("text");
 const ratingSelection = document.getElementById("rating-selection");
 const submitDiv = document.getElementById("submit");
 
 function selectRatingNumberOne() {
-    one.style.backgroundColor = " hsl(25, 97%, 53%)";
+    one.style.backgroundColor = "hsl(25, 97%, 53%)";
     one.style.color = "white";
 }
 
 function selectRatingNumberTwo() {
-    two.style.backgroundColor = " hsl(25, 97%, 53%)";
+    two.style.backgroundColor = "hsl(25, 97%, 53%)";
     two.style.color = "white";
 }
 
 function selectRatingNumberThree() {
-    three.style.backgroundColor = " hsl(25, 97%, 53%)";
+    three.style.backgroundColor = "hsl(25, 97%, 53%)";
     three.style.color = "white";
 }
 
 function selectRatingNumberFour() {
-    four.style.backgroundColor = " hsl(25, 97%, 53%)";
+    four.style.backgroundColor = "hsl(25, 97%, 53%)";
     four.style.color = "white";
 }
 
 function selectRatingNumberFive() {
-    five.style.backgroundColor = " hsl(25, 97%, 53%)";
+    five.style.backgroundColor = "hsl(25, 97%, 53%)";
     five.style.color = "white";
 }
 
@@ -38,6 +40,14 @@ function selectRatingNumberFive() {
 
 function submit() {
     star.remove();
+    thankYouImg.style.display ="block";
+    thankYouImg.style.margin = "1px auto"
+    yourSelection.innerHTML = `You selected  out of 5`;
+    yourSelection.style.textAlign = "center";
+    yourSelection.style.backgroundColor = "hsl(227, 8%, 23%)";
+    yourSelection.style.borderRadius = "20px"
+    yourSelection.style.padding = "auto 2px";
+    yourSelection.style.color = "hsl(25, 97%, 53%)";
     title.innerHTML = "Thank you!";
     title.style.textAlign = "center";
     text.innerHTML = "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!"
