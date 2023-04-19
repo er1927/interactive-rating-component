a7510bd9d94b9ef4eee88f4eea7e90640c983324
Elizabeth R <elizabeth.roberts1927@gmail.com>
2023-04-19 13:56:38 -0300

    Added function to change background color on selected rating.

---
+++ script.js
@@ -0,0 +1,32 @@
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+
+
+function selectRatingNumberOne() {
+    one.style.backgroundColor = " hsl(25, 97%, 53%)";
+    one.style.color = "white";
+}
+
+function selectRatingNumberTwo() {
+    two.style.backgroundColor = " hsl(25, 97%, 53%)";
+    two.style.color = "white";
+}
+
+function selectRatingNumberThree() {
+    three.style.backgroundColor = " hsl(25, 97%, 53%)";
+    three.style.color = "white";
+}
+
+function selectRatingNumberFour() {
+    four.style.backgroundColor = " hsl(25, 97%, 53%)";
+    four.style.color = "white";
+}
+
+function selectRatingNumberFive() {
+    five.style.backgroundColor = " hsl(25, 97%, 53%)";
+    five.style.color = "white";
+}
+
