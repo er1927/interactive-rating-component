38e84f15a793b036269c13d5b378abcb416af2bf
Elizabeth R <elizabeth.roberts1927@gmail.com>
2023-04-19 14:23:43 -0300

    Added functions to show thank you card title and text, remove star, rating selection and submit button.

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const two = document.getElementById("two");
 const three = document.getElementById("three");
 const four = document.getElementById("four");
 const five = document.getElementById("five");
-
+const star = document.getElementById("star-container");
+const title= document.getElementById("title");
+const text = document.getElementById("text");
+const ratingSelection = document.getElementById("rating-selection");
+const submitDiv = document.getElementById("submit");
 
 function selectRatingNumberOne() {
     one.style.backgroundColor = " hsl(25, 97%, 53%)";
@@ -28,5 +32,16 @@ function selectRatingNumberFour() {
 function selectRatingNumberFive() {
     five.style.backgroundColor = " hsl(25, 97%, 53%)";
     five.style.color = "white";
+}
+
+
+
+function submit() {
+    star.remove();
+    title.innerHTML = "Thank you!";
+    title.style.textAlign = "center";
+    text.innerHTML = "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!"
+    ratingSelection.remove();
+    submitDiv.remove();
 }
 
